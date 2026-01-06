fd28f901699ffdaad12c6eedd4bfafd811309857
shivamkarn <shivamkarn767@gmail.com>
2026-01-06 19:30:21 +0545

    IMproved asynchandler

--- src/utils/AsyncHandler.ts
+++ src/utils/AsyncHandler.ts
@@ -1,25 +1,13 @@
 import type { RequestHandler, Request, Response, NextFunction } from "express";
 
-export type asyncHandler = <T>(
+export type AsyncHandlerFunction = (
   req: Request,
   res: Response,
   next: NextFunction
-) => Promise<T>;
+) => Promise<void>;
 
-const errorConstructor = (error: unknown): Error => {
-  return error instanceof Error ? error : new Error(String(error));
-};
-
-export type ErrorForwarder = (error: Error) => void;
-
-const forwardError = (nextFn: NextFunction, error: unknown) => {
-  nextFn(errorConstructor(error));
-};
-
-export const AsyncHandler = (handler: asyncHandler): RequestHandler => {
-  return (req, res, next) => {
-    void handler(req, res, next).catch((error: unknown) => {
-      forwardError(next, error);
-    });
+export const AsyncHandler = (handler: AsyncHandlerFunction): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    Promise.resolve(handler(req, res, next)).catch(next);
   };
 };
