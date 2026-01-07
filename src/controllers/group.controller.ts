e32ece34d68eb61b569b72df50ee7c5aaa077393
shivamkarn <shivamkarn767@gmail.com>
2026-01-07 12:07:09 +0545

    Added controller names in list

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -6,7 +6,18 @@ import { AsyncHandler } from "../utils/AsyncHandler";
 import { AppAssert } from "../utils/AppAssert";
 import { STATUS_CODE } from "../types/httpStatus.ts";
 import { ApiResponse } from "../utils/ApiResponse.ts";
-
+// createGroup
+//getUserGroups
+//getPublicGroups
+// getGroupById
+//getGroupMember
+//updateGroup
+//deleteGroup
+//inviteMember
+//acceptInvitation
+//getUserInvitations
+//declineInvitation
+// removeMember
 export class GroupController {
   createGroup = AsyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
@@ -29,212 +40,326 @@ export class GroupController {
   );
 
   getUserGroups = AsyncHandler(
-    async(req: Request, res : Response, next : NextFunction)=>{
+    async (req: Request, res: Response, next: NextFunction) => {
       if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication required");
       }
-    const userGroups = await groupService.getUserGroups(req.user.userId)
+      const userGroups = await groupService.getUserGroups(req.user.userId);
 
-    res.status(STATUS_CODE.SUCCESS).json(
-        new ApiResponse(STATUS_CODE.SUCCESS,userGroups,"The group's in which the given user is present is returned")
-      )
-    });
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            userGroups,
+            "The group's in which the given user is present is returned"
+          )
+        );
+    }
+  );
 
   getPublicGroups = AsyncHandler(
-    async(req: Request, res : Response, next : NextFunction)=>{
+    async (req: Request, res: Response, next: NextFunction) => {
       const userId = req.user?.userId;
 
       const publicGroups = await groupService.getNotJoinedGroups(userId);
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,publicGroups, "Groups that user are not in Returned")
-      )
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            publicGroups,
+            "Groups that user are not in Returned"
+          )
+        );
     }
-
-  )
+  );
 
   getGroupById = AsyncHandler(
-    async(req: Request, res : Response, next : NextFunction)=>{
-      const {groupId}= req.params; 
-      const userId = req.user?.userId ;
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { groupId } = req.params;
+      const userId = req.user?.userId;
 
-      if(!groupId){
-        throw new ApiError(STATUS_CODE.UNAUTHORIZED,"Group Id not provided");
+      if (!groupId) {
+        throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Group Id not provided");
       }
       const group = await groupService.getGroupById(groupId, userId);
 
-      if(!group){
-        throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication required, Group not found");
+      if (!group) {
+        throw new ApiError(
+          STATUS_CODE.UNAUTHORIZED,
+          "Authentication required, Group not found"
+        );
       }
 
-      res.status(STATUS_CODE.SUCCESS).json(
-        new ApiResponse(STATUS_CODE.SUCCESS,group,"Group with this ID returned")
-      ) ;
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            group,
+            "Group with this ID returned"
+          )
+        );
     }
   );
 
   getGroupMember = AsyncHandler(
-    async(req : Request, res  : Response, next: NextFunction)=>{
-      const {groupId} = req.params ; 
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { groupId } = req.params;
 
-      AppAssert(groupId, STATUS_CODE.NOT_FOUND,"Group with this id not found");
-      if(req.user){
-        const isMember = await permissionService.isMember(req.user.userId, groupId);
-        if(!isMember){
-          throw new ApiError(STATUS_CODE.FORBIDDEN,"You must be a member of this Group");
+      AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this id not found");
+      if (req.user) {
+        const isMember = await permissionService.isMember(
+          req.user.userId,
+          groupId
+        );
+        if (!isMember) {
+          throw new ApiError(
+            STATUS_CODE.FORBIDDEN,
+            "You must be a member of this Group"
+          );
         }
       }
       const userId = req.user?.userId;
-      if(!userId){
-        throw new ApiError(STATUS_CODE.NOT_FOUND,"User with this Id not found");
+      if (!userId) {
+        throw new ApiError(
+          STATUS_CODE.NOT_FOUND,
+          "User with this Id not found"
+        );
       }
-      const members  = await groupService.getGroupMembers(userId, groupId)
-      res.status(STATUS_CODE.SUCCESS).json(
-        new ApiResponse(STATUS_CODE.SUCCESS, members, "Group members returned")
-      )
+      const members = await groupService.getGroupMembers(userId, groupId);
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            members,
+            "Group members returned"
+          )
+        );
     }
   );
 
   updateGroup = AsyncHandler(
-    async (req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
-        throw new ApiError(STATUS_CODE.UNAUTHORIZED,"Authentication Required");
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
+        throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
 
-      const {groupId} = req.params;
+      const { groupId } = req.params;
 
-      AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this user id not found");
+      AppAssert(
+        groupId,
+        STATUS_CODE.NOT_FOUND,
+        "Group with this user id not found"
+      );
 
+      const updatedGroup = await groupService.updateGroup(
+        groupId,
+        req.user?.userId,
+        req.body
+      );
 
-      const updatedGroup = await groupService.updateGroup(groupId, req.user?.userId, req.body);
-
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,updatedGroup,"Group Updated"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(STATUS_CODE.SUCCESS, updatedGroup, "Group Updated")
+        );
     }
-  )
+  );
 
   deleteGroup = AsyncHandler(
-
-    async (req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
-        throw new ApiError(STATUS_CODE.UNAUTHORIZED,"Authentication Required");
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
+        throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
-      const {groupId} = req.params;
-      AppAssert(groupId,STATUS_CODE.NOT_FOUND,"Group with this id not found");
+      const { groupId } = req.params;
+      AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this id not found");
 
       await groupService.deleteGroup(groupId, req.user.userId);
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,{},"Group deleted Successfully"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(STATUS_CODE.SUCCESS, {}, "Group deleted Successfully")
+        );
     }
-  )
+  );
 
   inviteMember = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
-        throw new ApiError(STATUS_CODE.UNAUTHORIZED,"Authentication required");
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
+        throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication required");
       }
 
-      const {groupId} = req.params;
-      AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this id doesn't exist");
-      const {inviteeEmail, roleId} = req.body;
+      const { groupId } = req.params;
+      AppAssert(
+        groupId,
+        STATUS_CODE.NOT_FOUND,
+        "Group with this id doesn't exist"
+      );
+      const { inviteeEmail, roleId } = req.body;
 
+      const result = await groupService.inviteUserToGroup(
+        groupId,
+        req.user.userId,
+        inviteeEmail,
+        roleId
+      );
 
-      const result = await groupService.inviteUserToGroup(groupId, req.user.userId, inviteeEmail, roleId);
-
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result,"User Invitation Sent"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(STATUS_CODE.SUCCESS, result, "User Invitation Sent")
+        );
     }
   );
 
   acceptInvitation = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
 
-      const {token} = req.params;
-      if(!token){
+      const { token } = req.params;
+      if (!token) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Token not provided");
       }
 
-      const result = await groupService.acceptinvitation(token, req.user?.userId);
+      const result = await groupService.acceptinvitation(
+        token,
+        req.user?.userId
+      );
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "Invitation Accepted" ))
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(STATUS_CODE.SUCCESS, result, "Invitation Accepted")
+        );
     }
   );
 
   declineInvitation = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
 
-      const {token} = req.params;
-      if(!token){
+      const { token } = req.params;
+      if (!token) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Token not provided");
       }
 
-      const result = await groupService.declineInvitation(token, req.user?.userId);
+      const result = await groupService.declineInvitation(
+        token,
+        req.user?.userId
+      );
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "Invitation Declined" ))
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(STATUS_CODE.SUCCESS, result, "Invitation Declined")
+        );
     }
   );
 
   getUserInvitations = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
 
       const result = await groupService.getuserinvitations(req.user.userId);
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "User Invitations served"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            result,
+            "User Invitations served"
+          )
+        );
     }
   );
 
   removeMember = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
 
       const userWhoIsRemoving = req.user.userId;
-      const {userWhoisBeingRemoved , groupId} = req.body;
+      const { userWhoisBeingRemoved, groupId } = req.body;
 
-      const result = await groupService.removeMember(groupId, userWhoIsRemoving, userWhoIsRemoving);
+      const result = await groupService.removeMember(
+        groupId,
+        userWhoIsRemoving,
+        userWhoIsRemoving
+      );
 
-
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "User removed from Group"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            result,
+            "User removed from Group"
+          )
+        );
     }
   );
 
   leaveGroup = AsyncHandler(
-    async(req : Request, res : Response, next : NextFunction)=>{
-      if(!req.user){
+    async (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
         throw new ApiError(STATUS_CODE.UNAUTHORIZED, "Authentication Required");
       }
-      const {groupId} = req.params;
+      const { groupId } = req.params;
 
       AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this id not found");
       const result = await groupService.leaveGroup(groupId, req.user?.userId);
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "Group Leaved"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(new ApiResponse(STATUS_CODE.SUCCESS, result, "Group Leaved"));
     }
   );
 
   updateMemberRole = AsyncHandler(
-
-    async(req : Request, res : Response, next : NextFunction)=>{
+    async (req: Request, res: Response, next: NextFunction) => {
       if (!req.user) {
-        throw new ApiError(401, 'Authentication required');
-      };
+        throw new ApiError(401, "Authentication required");
+      }
 
-      const {groupId, memberId} = req.params;
+      const { groupId, memberId } = req.params;
 
-      const {roleId } = req.body;
-      AppAssert(memberId, STATUS_CODE.NOT_FOUND, "Member with this id not found");
+      const { roleId } = req.body;
+      AppAssert(
+        memberId,
+        STATUS_CODE.NOT_FOUND,
+        "Member with this id not found"
+      );
       AppAssert(roleId, STATUS_CODE.NOT_FOUND, "Role with this id not found");
       AppAssert(groupId, STATUS_CODE.NOT_FOUND, "Group with this id not found");
-      const result = await groupService.updateMemberRole(groupId, req.user?.userId, memberId, roleId);
+      const result = await groupService.updateMemberRole(
+        groupId,
+        req.user?.userId,
+        memberId,
+        roleId
+      );
 
-      res.status(STATUS_CODE.SUCCESS).json(new ApiResponse(STATUS_CODE.SUCCESS,result, "Member role updated successfully"));
+      res
+        .status(STATUS_CODE.SUCCESS)
+        .json(
+          new ApiResponse(
+            STATUS_CODE.SUCCESS,
+            result,
+            "Member role updated successfully"
+          )
+        );
     }
   );
 }
