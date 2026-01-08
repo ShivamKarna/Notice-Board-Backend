becefc5eaf06b210cfcb6ad84ba8f09ebbd04131
shivamkarn <shivamkarn767@gmail.com>
2026-01-08 11:46:26 +0545

    Added cloudinary config

---
+++ src/config/cloudinary.config.ts
@@ -0,0 +1,47 @@
+import { v2  as cloudinary} from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+export const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: 'noticeboard/posts',
+      allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp'],
+      transformation: [
+        { width: 1200, height: 1200, crop: 'limit' }, // Max dimensions
+        { quality: 'auto' }, 
+      ],
+    };
+  },
+});
+
+export async function deleteImage(publicId: string): Promise<void> {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.error('Error deleting image from Cloudinary:', error);
+    throw error;
+  }
+}
+
+// get public_url from cloudinary
+export function getPublicIdFromUrl(url: string) {
+  try {
+    const matches = url.match(/\/v\d+\/(.+)\.\w+$/);
+    return matches ? matches[1] : null;
+  } catch (error) {
+    return null;
+  }
+}
+
+
+export {cloudinary};
