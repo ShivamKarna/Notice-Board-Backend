25c77c7f5abc470deec311bb676fcebf14c7d747
shivamkarn <shivamkarn767@gmail.com>
2026-01-08 11:47:39 +0545

    Added structure for PostServices

---
+++ src/services/post.services.ts
@@ -0,0 +1,21 @@
+
+
+export class PostServices{
+  // Things to do 
+  // Create a new post (draft mode)
+  //submitPostForApproval
+  //approvePost
+  //rejectPost
+  //updatePost
+  //deletePost
+  //getPostById
+  //getGroupPosts
+  //getPendingPosts
+  //getUserPosts
+  //addMediaToPost
+  //removeMediaFromPost
+
+}
+
+
+export const postServices = new PostServices();
