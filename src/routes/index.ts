115542986f5d9258b0bf0dde54c9d0bee3da65e3
shivamkarn <shivamkarn767@gmail.com>
2026-01-11 06:34:25 +0545

    Imported route in main(index.ts) file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { authRouter } from "./auth/auth.routes";
 import { cronRouter } from "./cron.routes";
 import { groupRouter } from "./group/group.routes";
-
+import { postRouter } from "./post/post.routes";
 const router = Router();
+
+// prefix :   /api/v1/
 
 router.use("/auth", authRouter);
 router.use("/cron", cronRouter);
 router.use("/group", groupRouter);
+router.use("/posts",postRouter);
 
 router.get("/health", (req, res) => {
   res.json({
